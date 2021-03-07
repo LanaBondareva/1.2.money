2008e74db942f391b1dafb4ea7b10b0101ffc18a
Svetlana Bondareva <b.s.a94@mail.ru>
2021-03-07 13:20:13 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentbalance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = currentbalance + transfer;
+        System.out.println("Total: " + total);
+    }
+}
